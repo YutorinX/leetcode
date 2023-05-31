54573d6d862848e2c11d2e5cd9c7335d2246d357
Yutorin <yutorinx@gmail.com>
2023-05-31 16:32:46 +0900

    ⚡️ : add new solution

--- src/Running_Sum_of_1d_Array_1480.rs
+++ src/Running_Sum_of_1d_Array_1480.rs
@@ -10,3 +10,16 @@ impl Solution {
         return output;
     }
 }
+
+impl Solution2 {
+    pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+        let mut output = vec![nums[0]];
+
+        for index in 1..nums.len() {
+            let last_value = output[index - 1];
+            output.push(last_value + nums[index]);
+        }
+
+        return output;
+    }
+}
